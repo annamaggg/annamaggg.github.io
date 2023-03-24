8dd354e6dc763c69815e6cec8c648ef3ceddb4d2
annamaggg <annamagnusson99@gmail.com>
2023-03-24 17:44:03 +0000

    jsx tags added for first feature in software page

--- src/components/Software.js
+++ src/components/Software.js
@@ -6,7 +6,13 @@ export const Software = () => {
     <>
       <Navbar />
       <div>
-        <h1>This is the software page</h1>
+        <h1>Burn Book</h1>
+        <p>Created a social media web application in a group of six with a mean girls 
+        theme. Users can sign up, log in, post, comment and like posts. The app 
+        was designed using Trello boards, Excalidraw and Photoshop.
+        It was built using MongoDB database, JavaScript with React framework, 
+        and TailwindCSS.</p>
+        <a href='https://github.com/SimpleLuke/acebook-fire' target="blank"><p>View the code</p></a>
       </div>
     </>
   )
