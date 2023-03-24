20f78ce4fe40970b9c7246313e56017decc9d79b
annamaggg <annamagnusson99@gmail.com>
2023-03-24 18:08:12 +0000

    software page element styling added

--- src/components/Software.js
+++ src/components/Software.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Navbar } from './Navbar';
+import './Software.css';
+import BurnBook from '../content/BurnBook.png';
 
 export const Software = () => {
   return (
     <>
       <Navbar />
-      <div>
-        <h1>Burn Book</h1>
-        <p>Created a social media web application in a group of six with a mean girls 
-        theme. Users can sign up, log in, post, comment and like posts. The app 
-        was designed using Trello boards, Excalidraw and Photoshop.
-        It was built using MongoDB database, JavaScript with React framework, 
-        and TailwindCSS.</p>
-        <a href='https://github.com/SimpleLuke/acebook-fire' target="blank"><p>View the code</p></a>
+      <div className='container'>
+        <div className='textElements'>
+          <h1>Burn Book</h1>
+          <p>Created a social media web application in a group of six with a Mean Girls 
+          theme. Users can sign up, log in, post, comment and like posts. The app 
+          was designed using Trello boards, Excalidraw and Photoshop.
+          It was built using MongoDB database, JavaScript with React framework, 
+          and TailwindCSS.</p>
+          <a href='https://github.com/SimpleLuke/acebook-fire' target="blank"><p>View the code</p></a>
+        </div>
+        <div className='image'>
+          <img src={BurnBook} alt='Burnbook'/>
+        </div>
       </div>
     </>
   )
