cb820d450fd87c1d1d60f228d45f6d48025d22bf
annamaggg <annamagnusson99@gmail.com>
2023-05-03 18:12:49 +0100

    Hash router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-//import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Software } from './components/Software';
